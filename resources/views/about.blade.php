6ee36ff83c53effefe6c16bc0d5310318445fc15
Alif Rusdianto <alifrusdianto30@gmail.com>
2024-07-18 11:33:23 +0700

    feat(blade): add view for home, news, about, and contact menu

---
+++ resources/views/about.blade.php
@@ -0,0 +1,11 @@
+<x-layout>
+    <x-slot:title>{{ $title }}</x-slot:title>
+    <p>
+        Welcome to <strong>New News</strong>, your go-to source for reliable, up-to-the-minute news coverage from around
+        the globe. Founded with the mission to provide unbiased, comprehensive news reporting, we strive to deliver
+        stories that matter to you. Our dedicated team of journalists, editors, and contributors work around the clock
+        to bring you the latest in politics, business, technology, sports, entertainment, and more. At <strong>New
+            News</strong>, we believe in the power of information and the importance of a well-informed public. Join us
+        as we navigate the fast-paced world of news, offering insightful analysis and in-depth reporting you can trust.
+    </p>
+</x-layout>
