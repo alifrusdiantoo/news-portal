2ef40a0f575134f36a19da0abd8151d3bdc751e7
Alif Rusdianto <alifrusdianto30@gmail.com>
2024-07-25 17:33:59 +0700

    feat(dashboard): add pagination in articles table

--- app/Http/Controllers/DashboardArticleController.php
+++ app/Http/Controllers/DashboardArticleController.php
@@ -15,7 +15,7 @@ class DashboardArticleController extends Controller
      */
     public function index()
     {
-        $articles = Article::where('author_id', auth()->user()->id)->orderByDesc('created_at')->get();
+        $articles = Article::where('author_id', auth()->user()->id)->latest()->paginate(10)->withQueryString();
 
         return view('dashboard.articles.index', [
             'articles' => $articles
--- resources/views/dashboard/articles/index.blade.php
+++ resources/views/dashboard/articles/index.blade.php
@@ -34,16 +34,6 @@
 								</svg>
 								Actions
 							</button>
-							<div id="actionsDropdown" class="hidden z-10 w-44 bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700 dark:divide-gray-600">
-								<ul class="py-1 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="actionsDropdownButton">
-									<li>
-										<a href="#" class="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Mass Edit</a>
-									</li>
-								</ul>
-								<div class="py-1">
-									<a href="#" class="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Delete all</a>
-								</div>
-							</div>
 						</div>
 					</div>
 				</div>
@@ -147,48 +137,12 @@
 							@endforelse
 						</tbody>
 					</table>
+
+					<!-- Pagination -->
+					<div class="my-4">
+						{{ $articles->links() }}
+					</div>
 				</div>
-				<nav class="flex flex-col md:flex-row justify-between items-start md:items-center space-y-3 md:space-y-0 p-4" aria-label="Table navigation">
-					<span class="text-sm font-normal text-gray-500 dark:text-gray-400">
-						Showing
-						<span class="font-semibold text-gray-900 dark:text-white">1-10</span>
-						of
-						<span class="font-semibold text-gray-900 dark:text-white">1000</span>
-					</span>
-					<ul class="inline-flex items-stretch -space-x-px">
-						<li>
-							<a href="#" class="flex items-center justify-center h-full py-1.5 px-3 ml-0 text-gray-500 bg-white rounded-l-lg border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
-								<span class="sr-only">Previous</span>
-								<svg class="w-5 h-5" aria-hidden="true" fill="currentColor" viewbox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-									<path fill-rule="evenodd" d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z" clip-rule="evenodd" />
-								</svg>
-							</a>
-						</li>
-						<li>
-							<a href="#" class="flex items-center justify-center text-sm py-2 px-3 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">1</a>
-						</li>
-						<li>
-							<a href="#" class="flex items-center justify-center text-sm py-2 px-3 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">2</a>
-						</li>
-						<li>
-							<a href="#" aria-current="page" class="flex items-center justify-center text-sm z-10 py-2 px-3 leading-tight text-primary-600 bg-primary-50 border border-primary-300 hover:bg-primary-100 hover:text-primary-700 dark:border-gray-700 dark:bg-gray-700 dark:text-white">3</a>
-						</li>
-						<li>
-							<a href="#" class="flex items-center justify-center text-sm py-2 px-3 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">...</a>
-						</li>
-						<li>
-							<a href="#" class="flex items-center justify-center text-sm py-2 px-3 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">100</a>
-						</li>
-						<li>
-							<a href="#" class="flex items-center justify-center h-full py-1.5 px-3 leading-tight text-gray-500 bg-white rounded-r-lg border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
-								<span class="sr-only">Next</span>
-								<svg class="w-5 h-5" aria-hidden="true" fill="currentColor" viewbox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-									<path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd" />
-								</svg>
-							</a>
-						</li>
-					</ul>
-				</nav>
 			</div>
 		</div>
 	</section>
