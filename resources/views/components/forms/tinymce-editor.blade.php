df4a253de134fe8e528673b690b018e393d6579c
Alif Rusdianto <alifrusdianto30@gmail.com>
2024-07-25 07:04:45 +0700

    fix(dashboard): null old value in create article content

--- resources/views/components/forms/tinymce-editor.blade.php
+++ resources/views/components/forms/tinymce-editor.blade.php
@@ -1,1 +1,1 @@
-<textarea id="myeditorinstance" name="{{ $slot }}">{{ old('content', $oldValue) }}</textarea>
+<textarea id="myeditorinstance" name="{{ $slot }}">{{ old('content', $oldValue ?? '') }}</textarea>
